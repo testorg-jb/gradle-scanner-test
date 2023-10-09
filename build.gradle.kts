7bdcf8ae068f5c9f2e7e3abb1df8d2fb64789a2c
Johann Beleites <johann.beleites@sonarsource.com>
2023-10-09 14:23:12 +0200

    Initial project configuration

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("org.sonarqube") version "4.3.1.3277"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+sonar {
+    properties {
+        property "sonar.projectKey", "testorg-jb_jackrabbit"
+        property "sonar.organization", "testorg-jb"
+        property "sonar.host.url", "https://sonarcloud.io"
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.api-ads:adwords-axis:4.20.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "gradle-scanner-test"
+
